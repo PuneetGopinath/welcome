c321a0acc91379a6cb2287fea9a1561c11756898
Puneet Gopinath <baalkrshna@gmail.com>
2021-05-30 13:42:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require("./config.json");
 const client = new Discord.Client();
+const prefix = "!";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -8,6 +9,18 @@ client.on('ready', () => {
 
 client.on("guildMemberAdd", (member) => {
     member.guild.channels.get('channelID').send("Welcome");
+});
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+  if (command === "ping") {
+    message.reply(`Pong!`);
+  }
 });
 
 client.login(config.BOT_TOKEN);
