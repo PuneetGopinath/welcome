367e13b569083d4a6ce2986f9ead72b084421ddb
Puneet Gopinath <baalkrshna@gmail.com>
2021-05-31 16:09:37 +0530

    Create presence.js

---
+++ src/functions/presence.js
@@ -0,0 +1,12 @@
+const presence = function () {
+    const servers = client.guilds.cache.size;
+    console.log(`Updating presence. Servers: ${servers}`);
+    client.user
+        .setPresence({
+            activity: {
+                name: `${servers} server${servers > 1 ? "s" : ""}`,
+                type: "WATCHING",
+            },
+        })
+        .catch((error) => console.error(error));
+};
