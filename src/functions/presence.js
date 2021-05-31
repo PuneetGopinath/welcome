79c680fa3ee12c63e8c347dfc902e85b29d2f9c4
Puneet Gopinath <baalkrshna@gmail.com>
2021-05-31 18:36:44 +0530

    Update presence.js

--- src/functions/presence.js
+++ src/functions/presence.js
@@ -1,3 +1,8 @@
+/**
+ * Discord Welcome bot
+ * Copyright (c) 2021 The BaalKrshna Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
 const presence = function (client) {
     const servers = client.guilds.cache.size;
     console.log(`Updating presence. Servers: ${servers}`);
