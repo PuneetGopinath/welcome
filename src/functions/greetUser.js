034bbb904286207980454890db17a08ef7091f40
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-05-31 16:16:59 +0530

    separated modules

---
+++ src/functions/greetUser.js
@@ -0,0 +1,12 @@
+const greetUser = function (guild, member) {
+    let channel;
+    channel = guild.channels.cache.find((ch) => ch.name === "new-members");
+    if (!channel) {
+        channel = guild.channels.cache.find((ch) => ch.name === "general");
+    }
+    if (!channel) return;
+    console.log("channel is set");
+    channel.send(`Welcome, ${member}`);
+};
+
+module.exports = greetUser
