2671bf82d6bec3825d3085ad0b95ee0f88a2faaf
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-05-31 16:26:35 +0530

    Update src/app.js
    Co-authored-by: Puneet Gopinath <puneetgopinath2007@gmail.com>

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@
  */
 const Discord = require("discord.js");
 
-const presence = require('./functions/presence')
+const presence = require('./functions/presence');
 const greetUser = require('./functions/greetUser');
 
 if (!process.env.BOT_TOKEN) {
